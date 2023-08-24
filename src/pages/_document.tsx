a0a7c8e9b7ab7dcd10b258d16b29172649bda436
Desmond Delali <datakpla@gmail.com>
2023-08-23 19:13:04 -0700

    Done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,7 @@ export default function Document() {
           - https://github.com/facebook/react/issues/11538
           - https://bugs.chromium.org/p/chromium/issues/detail?id=872770 */}
         <meta content="notranslate" name="google" />
-        <link href="src/images/favicon.png" rel="icon" type="image/png"></link>
+        <link href="src/images/favicon.ico" rel="icon" type="image/x-icon"></link>
       </Head>
       <body className="bg-black">
         <Main />
