7c4eccf3553fc4618361fd8035d165b82aa75d94
Desmond Delali <datakpla@gmail.com>
2023-09-04 22:04:21 -0700

    pdf of cv updated

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -28,7 +28,7 @@ const Hero: FC = memo(() => {
               <Socials />
             </div>
             <div className="flex w-full justify-center gap-x-4">
-              {actions.map(({href, text, primary, Icon}) => (
+              {actions.map(({href, text, primary, Icon,}) => (
                 <a
                   className={classNames(
                     'flex gap-x-2 rounded-full border-2 bg-none px-4 py-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
